2eeb455d044bede60e70f3a6ef0b694586c14f47
Hashim Muhaned <hashimcode123@gmail.com>
2024-12-18 00:22:04 +0400

    fixing fetching bugs

--- backend/controllers/EcommerceCTRL.js
+++ backend/controllers/EcommerceCTRL.js
@@ -132,11 +132,17 @@ const createItem = async (req, res) => {
 
 const getItems = async (req, res) => {
   try {
+    console.log("Fetching items from the database...");
     const items = await ItemModel.find({});
-    // console.log(items);
-    res.status(201).json(items);
+
+    if (items.length === 0) {
+      return res.status(404).json({ message: "No items found" });
+    }
+
+    res.status(200).json(items);
   } catch (error) {
-    res.status(500).json({ message: "Server Error", error });
+    console.error("Error fetching items:", error);
+    res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
 
