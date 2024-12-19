931511f40b31cf57f9bfea248cc392aff9b11d9d
Hashim Muhaned <hashimcode123@gmail.com>
2024-12-19 23:53:00 +0400

    fixing the bucket issue

--- frontend/src/components/UploadImage.jsx
+++ frontend/src/components/UploadImage.jsx
@@ -14,7 +14,7 @@ const AddItemForm = () => {
     apiKey: process.env.apiKey,
     authDomain: process.env.authDomain,
     projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
+    storageBucket: "ethreal-1a9e9.firebasestorage.app",
     messagingSenderId: process.env.messagingSenderId,
     appId: process.env.appId,
     measurementId: process.env.measurementId,
@@ -24,7 +24,7 @@ const AddItemForm = () => {
   console.log("Storage Bucket:", firebaseConfig.storageBucket);
 
   const app = initializeApp(firebaseConfig);
-  const storage = getStorage(app, "ethreal-1a9e9.firebasestorage.app");
+  const storage = getStorage(app);
 
   const { setYourItems } = useContext(YourItemsContext);
   const { setData } = useContext(DataContext);
