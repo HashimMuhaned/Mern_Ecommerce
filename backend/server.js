784ebaeac6ac3de3681bea6b6eeada28d999d612
Hashim Muhaned <hashimcode123@gmail.com>
2024-12-21 07:44:33 +0400

    fixing cors

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:
-      "https://mern-ecommerce-frontend-lkugdd60x-hashims-projects-1d68b3df.vercel.app",
+    origin: "https://mern-ecommerce-frontend-murex.vercel.app",
     credentials: true, // If you're using cookies or other credentials
   })
 );
